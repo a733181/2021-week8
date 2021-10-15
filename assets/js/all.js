2c550f1d3897e69cfe4ba9e6d92afde9d6ab98ac
a733181 <a733181@gmail.com>
2021-10-15 17:13:58 +0800

    Update 2021-10-15T09:13:54.645Z

---
+++ assets/js/all.js
@@ -0,0 +1,83 @@
+"use strict";
+
+var swiper = new Swiper(".swiper-container", {});
+var customSwiper = new Swiper(".customSwiper", {
+  slidesPerView: 1,
+  slidesPerColumn: 3,
+  spaceBetween: 8,
+  slidesPerColumnFill: "row",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2.1,
+      slidesPerColumn: 2,
+      spaceBetween: 30
+    },
+    1200: {
+      slidesPerView: 3,
+      slidesPerColumn: 2,
+      spaceBetween: 30
+    }
+  }
+});
+var chooseSwiper = new Swiper(".chooseSwiper", {
+  slidesPerView: 1,
+  slidesPerColumn: 3,
+  spaceBetween: 20,
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+      slidesPerColumn: 1,
+      spaceBetween: 30
+    },
+    960: {
+      slidesPerView: 3,
+      slidesPerColumn: 1,
+      spaceBetween: 20
+    }
+  }
+});
+var teacherSwiper = new Swiper(".teacherSwiper", {
+  slidesPerView: 1,
+  slidesPerColumn: 4,
+  spaceBetween: 72,
+  slidesPerColumnFill: "row",
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      slidesPerColumn: 1,
+      spaceBetween: 30
+    },
+    1200: {
+      slidesPerView: 3,
+      slidesPerColumn: 1,
+      spaceBetween: 30
+    }
+  }
+});
+var courseSwiper = new Swiper(".courseSwiper", {
+  slidesPerView: 1.2,
+  slidesPerColumn: 1,
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      slidesPerColumn: 1
+    },
+    1200: {
+      slidesPerView: 4.2,
+      slidesPerColumn: 1
+    }
+  }
+});
+var arrow = document.querySelector('input[name="datepicker"]');
+var datepicker = new Datepicker(arrow, {
+  nextArrow: ">",
+  prevArrow: "<",
+  buttonClass: "btn primary"
+});
+//# sourceMappingURL=all.js.map
